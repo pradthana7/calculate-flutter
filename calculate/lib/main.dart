e2f00472657da8e620cc0b3ed19064d27e0b31cb
pradthana7 <pradthana797@gmail.com>
2023-02-07 01:59:30 +0700

    basic BMI

--- calculate/lib/main.dart
+++ calculate/lib/main.dart
@@ -29,19 +29,74 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController height = TextEditingController();
+  TextEditingController weight = TextEditingController();
+  TextEditingController result = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    result.text = "your height 0 your weight 0, so your BMI is 0";
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Image.asset('bmi.jpg'),
-          Text("BMI calculate", style: TextStyle(fontSize: 30),),
-          TextField(
-            decoration: InputDecoration(
-                labelText: 'height', border: OutlineInputBorder()),
-          )
-        ],
-      ),
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Center(
+            child: Column(
+              children: [
+                Image.asset('bmi.jpg'),
+                Text(
+                  "BMI calculate",
+                  style: TextStyle(fontSize: 30),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: height,
+                  decoration: InputDecoration(
+                      labelText: 'height (cm)', border: OutlineInputBorder()),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: weight,
+                  decoration: InputDecoration(
+                      labelText: 'weight (kg)', border: OutlineInputBorder()),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    var _height = double.parse(height.text) / 100;
+                    var cal =
+                        ((double.parse(weight.text) / (_height * _height)))
+                            .toStringAsFixed(2);
+
+                    setState(() {
+                      result.text =
+                          "heigh: ${height.text} cm your weight: ${weight.text} kg, and your BMI is $cal";
+                    });
+                    print("height: ${height.text}");
+                    print("weight: ${weight.text}");
+                  },
+                  child: Text("calculate"),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Color(0xff00ced1)),
+                      padding: MaterialStateProperty.all(
+                          EdgeInsets.fromLTRB(50, 20, 50, 20)),
+                      textStyle:
+                          MaterialStateProperty.all(TextStyle(fontSize: 20))),
+                ),
+                SizedBox(height: 10),
+                Text(result.text),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
